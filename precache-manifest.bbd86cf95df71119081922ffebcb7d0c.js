6bfac0dcd08079e83ad1071f38242607cb7c1bee
Yan Bavota <ybavo@hotmail.com>
2019-01-16 03:18:53 -0500

    Updates

---
+++ precache-manifest.bbd86cf95df71119081922ffebcb7d0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "391c3ac23e2a97a6d232",
+    "url": "/youtube-search/static/js/runtime~main.391c3ac2.js"
+  },
+  {
+    "revision": "535a8bd92cca9eb499fe",
+    "url": "/youtube-search/static/js/main.535a8bd9.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/youtube-search/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "535a8bd92cca9eb499fe",
+    "url": "/youtube-search/static/css/main.d8c7cb60.chunk.css"
+  },
+  {
+    "revision": "db5fadf53ce2b3734be6df8702ee1a0f",
+    "url": "/youtube-search/index.html"
+  }
+];
