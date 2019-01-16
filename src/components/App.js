9e96166811ab2144bb67d8995bdbe4b826b0db5e
Yan Bavota <ybavo@hotmail.com>
2019-01-16 04:13:42 -0500

    Title Added

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
   };
 
   componentDidMount() {
-    this.onSearchSumit("Afv");
+    this.onSearchSumit("funny video");
   }
 
   onVideoSelect = video => {
